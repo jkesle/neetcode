10f712aadc60687450069bce97c6ef863c3c1318
Josh Kesler <joshua.e.kesler@gmail.com>
2022-07-19 21:07:07 -0500

    GroupAnagrams solution unoptimized

--- ArraysAndHashing/GroupAnagrams/Solution.cs
+++ ArraysAndHashing/GroupAnagrams/Solution.cs
@@ -8,9 +8,28 @@ namespace neetcode.ArraysAndHashing.GroupAnagrams
 {
     public class Solution
     {
-        public IList<IList<string>> GroupAnagrams(string[] strs)
+        public IList<IList<string>> GroupAnagrams(string[] strs) //TODO: optimize
         {
-            return null;
+
+            var res = new List<IList<string>>();
+            strs = strs.OrderBy<string, int>(s => s.Count()).ToArray();
+            var dict = new Dictionary<string, int>();
+            var index = 0;
+
+            foreach (var st in strs)
+            {
+                var s = new string(st.ToLower().ToCharArray().OrderBy(c => (byte)c).ToArray());
+                if (!dict.TryAdd(s, index))
+                {
+                    res[dict[s]].Add(st);
+                }
+                else
+                {
+                    res.Add(new List<string>(new string[] { st }));
+                    index++;
+                }
+            }
+            return res;
         }
     }
 }
