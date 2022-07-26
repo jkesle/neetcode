4a1055af8bd49fde2ddaf0f93942abe50506a2b1
Josh Kesler <joshua.e.kesler@gmail.com>
2022-07-25 22:40:10 -0500

    Update ValidPalindrome to use two pointer approach

--- TwoPointers/ValidPalindrome/Solution.cs
+++ TwoPointers/ValidPalindrome/Solution.cs
@@ -6,12 +6,28 @@ namespace neetcode.TwoPointers.ValidPalindrome
 {
     public class Solution
     {
+        //public bool ValidPalindrome(string s) //unoptimized
+        //{
+           // var regex = new Regex("[^A-Za-z0-9]");
+            //s = new string(s.ToLower().ToCharArray().Where(c => !regex.IsMatch(c.ToString())).ToArray());
+            //return new string(s.Reverse().ToArray()) == s;
+        //}
+
         public bool ValidPalindrome(string s)
         {
-            var regex = new Regex("[^A-Za-z0-9]");
-            s = new string(s.ToCharArray().Where(c => !regex.IsMatch(c.ToString())).ToArray());
-            Console.WriteLine(s);
-            return new string(s.Reverse().ToArray()) == s;
+            int l = 0, r = s.Length - 1;
+            var regex = new Regex("[\\W_]");
+            s = new string(s.ToLower().ToCharArray().Where(c => !regex.IsMatch(c.ToString())).ToArray()); //this could be optimized
+
+            while (l <= r)
+            {
+                if (s[l++] != s[r--])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
